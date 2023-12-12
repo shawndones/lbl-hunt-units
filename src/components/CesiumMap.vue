27cfaf626b1f5d66ec5f16475b6c6f5d3d4f8817
shawndones <shawndones@gmail.com>
2023-12-12 13:50:34 -0500

    first commit

--- src/components/CesiumMap.vue
+++ src/components/CesiumMap.vue
@@ -4,7 +4,7 @@
 
   // The URL on your server where CesiumJS's static files are hosted.
   window.CESIUM_BASE_URL = '/';
-  Ion.defaultAccessToken = '';
+  Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5YTdhZTRkMi1hOTI4LTQ2NTgtOWQzNS0zNjY3MDUyYTk0NjgiLCJpZCI6MTgzOTcyLCJpYXQiOjE3MDIzNzY1Nzh9.JtUBjc_dY1idwwzbAJrnUG__m_WiE6NXxI8rsgYmGC8';
 
 
 
@@ -28,12 +28,34 @@
       });
 
       this.viewer.camera.flyTo({
-        destination: Cartesian3.fromDegrees(-100.31, 47.47, 50000),
+        destination: Cartesian3.fromDegrees(-88.1265, 36.84, 81398),
         orientation: {
           heading: CesiumMath.toRadians(0.0),
           pitch: CesiumMath.toRadians(-90),
         }
       });
+
+      // createOsmBuildingsAsync().then(buildingTileset => {
+      //   this.viewer.scene.primitives.add(buildingTileset);
+      // });
+
+
+      // Load and add the KMZ file to the viewer
+      KmlDataSource.load('lbl-hunt-area-4.kml')  // Change this to the actual path of your KMZ file
+        .then(dataSource => {
+          this.viewer.dataSources.add(dataSource);
+
+          // Ensure there are entities in the dataSource before attempting to zoom
+          if (dataSource.entities.values.length > 0) {
+            const entities = dataSource.entities.values;
+            this.viewer.zoomTo(entities); // Zoom to all the entities
+          } else {
+            console.warn('No entities found in the KMZ file');
+          }
+        })
+        .catch(error => {
+          console.error('Error loading KMZ file:', error);
+        });
 
 
       // Update coordinates when the camera moves

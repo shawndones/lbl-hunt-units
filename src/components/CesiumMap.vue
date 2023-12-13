f8e470728d4c36dba8043228f053545e5bb29cc2
shawndones <shawndones@gmail.com>
2023-12-12 19:21:47 -0500

    add zone lables

--- src/components/CesiumMap.vue
+++ src/components/CesiumMap.vue
@@ -1,5 +1,5 @@
 <script>
-  import { Cartesian3, Math as CesiumMath, Terrain, Viewer, KmlDataSource, defined, ScreenSpaceEventType, Color, LabelStyle, VerticalOrigin, Cartographic, GroundPolylinePrimitive, ColorGeometryInstanceAttribute, GroundPolylineGeometry, GeometryInstance, PolylineColorAppearance } from 'cesium';
+  import * as Cesium from 'cesium';
   import 'cesium/Build/Cesium/Widgets/widgets.css';
 
   export default {
@@ -14,6 +14,7 @@
           longitude: 0,
           height: 0
         },
+        is2D: true, // Start with 2D terrain
         kmlLayers: [
           { name: 'LBL Hunt Areas', url: 'lbl-hunt-areas.kml', visible: true },
           { name: 'Elk Hunt Units', url: 'elk-hunt-units.kml', visible: true }
@@ -22,128 +23,123 @@
     },
 
     mounted() {
-      this.viewer = new Viewer(this.$refs.cesiumContainer, {
-        terrain: Terrain.fromWorldTerrain(),
+      this.viewer = new Cesium.Viewer(this.$refs.cesiumContainer, {
+        terrainProvider: new Cesium.EllipsoidTerrainProvider(),
+        // terrain: new Cesium.Terrain.fromWorldTerrain(),
       });
       this.viewer.camera.flyTo({
-        destination: Cartesian3.fromDegrees(-85.68, 37.05, 603560),
+        destination: Cesium.Cartesian3.fromDegrees(-85.68, 37.05, 603560),
         orientation: {
-          heading: CesiumMath.toRadians(0.0),
-          pitch: CesiumMath.toRadians(-90),
+          heading: Cesium.Math.toRadians(0.0),
+          pitch: Cesium.Math.toRadians(-90),
         }
       });
 
+      this.kmlLayers.forEach(layer => {
+        console.log(layer);
+        Cesium.KmlDataSource.load(layer.url)
+          .then(dataSource => {
+            this.viewer.dataSources.add(dataSource);
+            dataSource.show = layer.visible;
+            
+            console.log(dataSource.entities.values);
+            if (dataSource.entities.values.length > 0) {
+              const entities = dataSource.entities.values;
+              this.viewer.zoomTo(entities);
 
+              entities.forEach(entity => {
+                 // Access the properties
+      // const properties = entity.properties;
+      // console.log(entity)
+      // Extract your SimpleData fields
+      // const areaName = properties.AreaName.getValue();
+      // const unitNumb = properties.UnitNumb.getValue();
+      // const acres = properties.ACRES.getValue();
+      // console.log(areaName);
+      // console.log(unitNumb);
+      // console.log(acres);
 
-
-     // Load multiple KML files
-this.kmlLayers.forEach(layer => {
-  KmlDataSource.load(layer.url)
-    .then(dataSource => {
-      this.viewer.dataSources.add(dataSource);
-      dataSource.show = layer.visible;
-
-      if (dataSource.entities.values.length > 0) {
-        const entities = dataSource.entities.values;
-        this.viewer.zoomTo(entities);
-
-        entities.forEach(entity => {
-          // Check if the entity is a polygon
-          if (entity.polygon) {
-            // Extract the coordinates of the polygon
-            const polygonHierarchy = entity.polygon.hierarchy.getValue();
-            const coordinates = [];
-
-            polygonHierarchy.positions.forEach(position => {
-              const cartographic = Cartographic.fromCartesian(position);
-              coordinates.push(CesiumMath.toDegrees(cartographic.longitude));
-              coordinates.push(CesiumMath.toDegrees(cartographic.latitude));
-            });
-
-            // Create and add ground clamped polyline
-            const groundPolylinePrimitive = this.createGroundClampedPolyline(coordinates, Color.WHITE);
-            this.viewer.scene.primitives.add(groundPolylinePrimitive);
-
-            // Optionally, hide the original polygon
-            entity.show = false;
-          }
-
-
-              // Calculate the center of the entity
-              const boundingSphere = entity.boundingSphere;
-
-
-              if (boundingSphere) {
-
-                const center = Cartesian3.fromRadians(boundingSphere.center.longitude, boundingSphere.center.latitude, boundingSphere.center.height);
-
-                // Add a label to the entity
-                this.viewer.entities.add({
-                  position: center,
-                  label: {
-                    text: entity.name || 'Unnamed',
-                    font: '14pt monospace',
-                    fillColor: Color.WHITE,
-                    outlineColor: Color.BLACK,
-                    outlineWidth: 2,
-                    style: LabelStyle.FILL_AND_OUTLINE,
-                    verticalOrigin: VerticalOrigin.BOTTOM,
-                    pixelOffset: new Cartesian3(0, -15),
-                  }
-                });
-
-              }
-
-            });
-
-
+                if (entity.polygon) {
+                  entity.polygon.material = Cesium.Color.TRANSPARENT.withAlpha(0.5);
+                
+                  const positions = entity.polygon.hierarchy.getValue().positions;
+                  const boundingSphere = Cesium.BoundingSphere.fromPoints(positions);
+                  const center = boundingSphere.center;
+                  const centerCartographic = Cesium.Ellipsoid.WGS84.cartesianToCartographic(center);
+                
+                  this.viewer.entities.add({
+                    position: Cesium.Cartesian3.fromRadians(centerCartographic.longitude, centerCartographic.latitude, centerCartographic.height),
+                    label: {
+                      text: entity.name || 'Unnamed',
+                      font: '14pt monospace',
+                      fillColor: Cesium.Color.WHITE,
+                      outlineColor: Cesium.Color.BLACK,
+                      outlineWidth: 2,
+                      style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+                      verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+                      pixelOffset: new Cesium.Cartesian2(0, -15),
+                      
+                    }
+                  });
+                  console.log(entity);
+                }
+              });
             } else {
-            console.warn('No entities found in the KMZ file');
+              console.warn('No entities found in the KMZ file');
             }
-        })
-        .catch(error => console.error('Error loading KML file:', error));
-    });
+          })
+          .catch(error => console.error('Error loading KML file:', error));
+      });
 
-
-    // Add a hover event listener to the viewer
-    this.viewer.screenSpaceEventHandler.setInputAction(movement => {
+      this.viewer.screenSpaceEventHandler.setInputAction(movement => {
         const pickedObject = this.viewer.scene.pick(movement.endPosition);
-        if (defined(pickedObject) && pickedObject.id) {
+        if (Cesium.defined(pickedObject) && pickedObject.id) {
           this.selectedEntityInfo = pickedObject.id.name || 'No name';
           this.updateInfoBoxPosition(movement.endPosition);
         } else {
           this.selectedEntityInfo = null;
         }
-      }, ScreenSpaceEventType.MOUSE_MOVE);
+      }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
 
-      // Update coordinates when the camera moves
       this.viewer.camera.moveEnd.addEventListener(() => {
         this.updateCameraCoordinates();
       });
-
     },
 
     methods: {
+      toggleTerrain() {
+      if (this.is2D) {
+        // Switch to 3D terrain
+        this.viewer.terrainProvider = new Cesium.CesiumTerrainProvider({
+          url: 'https://assets.agi.com/stk-terrain/world', // Example terrain URL
+        });
+      } else {
+        // Switch back to 2D (flat) terrain
+        this.viewer.terrainProvider = new Cesium.EllipsoidTerrainProvider();
+      }
+      this.is2D = !this.is2D; // Toggle the state
+    },
       createGroundClampedPolyline(coordinates, color) {
-        const groundPolyline = new GroundPolylineGeometry({
-          positions: Cartesian3.fromDegreesArray(coordinates),
-          width: 2.0 // Set the desired width
+        const groundPolyline = new Cesium.GroundPolylineGeometry({
+          positions: Cesium.Cartesian3.fromDegreesArray(coordinates),
+          width: 2.0
         });
 
-        const groundPolylinePrimitive = new GroundPolylinePrimitive({
-          geometryInstances: new GeometryInstance({
+        const groundPolylinePrimitive = new Cesium.GroundPolylinePrimitive({
+          geometryInstances: new Cesium.GeometryInstance({
             geometry: groundPolyline,
             attributes: {
-              color: ColorGeometryInstanceAttribute.fromColor(color)
+              color: Cesium.ColorGeometryInstanceAttribute.fromColor(color)
             }
           }),
-          appearance: new PolylineColorAppearance({
+          appearance: new Cesium.PolylineColorAppearance({
             translucent: false
           })
         });
 
         return groundPolylinePrimitive;
       },
+
       toggleKmlLayer(layer) {
         const dataSource = this.viewer.dataSources.getByName(layer.name)[0];
         if (dataSource) {
@@ -151,6 +147,7 @@ this.kmlLayers.forEach(layer => {
           layer.visible = dataSource.show;
         }
       },
+
       updateInfoBoxPosition(screenPosition) {
         const infoBox = this.$refs.entityInfo;
         if (infoBox) {
@@ -158,29 +155,29 @@ this.kmlLayers.forEach(layer => {
           infoBox.style.top = `${screenPosition.y + 10}px`;
         }
       },
+
       updateCameraCoordinates() {
         const camera = this.viewer.camera;
         const position = camera.positionCartographic;
-
-        this.coordinates.latitude = CesiumMath.toDegrees(position.latitude);
-        this.coordinates.longitude = CesiumMath.toDegrees(position.longitude);
+        this.coordinates.latitude = Cesium.Math.toDegrees(position.latitude);
+        this.coordinates.longitude = Cesium.Math.toDegrees(position.longitude);
         this.coordinates.height = position.height;
       },
+
       getEntityInfo(entity) {
-        // Retrieve and return information from the entity
-        // For example, you can return entity.name or any other property
         return entity.name || 'No name';
       },
+
       recenterMap() {
         if (this.viewer && this.viewer.camera) {
           this.viewer.camera.flyTo({
-            destination: this.viewer.camera.position, // Keeps the current location
+            destination: this.viewer.camera.position,
             orientation: {
-              heading: 0, // North
-              pitch: CesiumMath.toRadians(-90), // Looking straight down
-              roll: 0 // No roll
+              heading: 0,
+              pitch: Cesium.Math.toRadians(-90),
+              roll: 0
             },
-            duration: 1.5 // Duration in seconds
+            duration: 1.5
           });
         }
       }
